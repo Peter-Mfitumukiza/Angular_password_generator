91df08201f7b6b90ce121a4f4503d75290fe1103
Mfitumukiza Peter <mfitep6@gmail.com>
2021-01-27 18:37:07 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  includeNumbers = false;
+  includeSymbols = false;
+  includeLetters = false;
+  length=undefined;
+  password = "";
+  useNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  useSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  useLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  getLength(input:string) {
+    const enteredNum = parseInt(input);
+    if (!isNaN(enteredNum)) {
+      this.length = enteredNum;
+      console.log(enteredNum);
+      
+    }
+  }
+  onGenerate() {
+    let numbers = "1234567890";
+    let chars = "asdfghjklpqwertyuiopzxcvbnm";
+    let symbols = "!@#$%^&*()<>?|{}+_-`";
+    let validChars = "";
+    let generatedPass = "";
+    if (this.includeLetters) {
+      validChars += chars;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+    for (let i = 0; i < this.length; i++){
+      let index = Math.floor(Math.random() * validChars.length);
+      generatedPass += validChars[index];
+    }
+    this.password = generatedPass;
+  }
 }
